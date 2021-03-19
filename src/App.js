d7f8f35eed7db3a639f5e4b14608b200a63b11d0
Victor Yodono <suunake@gmail.com>
2021-03-18 21:01:44 -0300

    App.js as a class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App(){
-  return(
-    <div>
-      <h1>This is a React application!</h1>
-    </div>
-  )
+import React, { Component } from 'react';
+
+class App extends Component {
+  render(){
+    return (
+        <div>
+          <h2>I'm now a class component! :]</h2>
+        </div>
+      );
+  }
 }
 
 export default App;
